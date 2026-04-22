0d05c6f2198219f2a680044f28ce46d882b2cf40
soeren <soeren.krumm@stud.uni-hannover.de>
2026-04-22 15:15:19 +0200

    ue5 kast part a4

--- bsn2/ue5/a4.c
+++ bsn2/ue5/a4.c
@@ -5,9 +5,27 @@
 #include <unistd.h>
 
 int main(){
-    pid_t pid; 
-    char text[100];  
-    for (int i = 0; i < 50; i++){
+    pid_t pid = fork(); 
+    //char text[100];  
+    for (int i = 0; i < 50; i++){ 
+        if (pid < 0){
+            perror("fork failure!!! abort!!"); 
+            exit(EXIT_FAILURE); 
+            break;
+        } else if(pid == 0){
+            printf("Kind %d\nFORK: %d\nPID: %d\nPPID: %d\n\n",i + 1,  pid, getpid(), getppid()); 
+            continue;       
+        }else{
+            printf("Eltern %d\nFORK: %d\nPID: %d\nPPID: %d\n\n",i + 1,  pid, getpid(), getppid()); 
+            pid = fork(); 
+            break; 
+        } 
+    }
+
+    return 0; 
+}
+
+/* lösung für 4 c 
         pid = fork(); 
         if (pid < 0){
             perror("fork failure!!! abort!!"); 
@@ -18,15 +36,11 @@ int main(){
             if (i == 49){
                 scanf("%s", text); 
                 printf("HAT GEKLAPPT: %s\n", text); 
-            }
-            sleep(20000); 
+            } 
             continue;       
         }else{
             printf("Eltern %d\nFORK: %d\nPID: %d\nPPID: %d\n\n",i + 1,  pid, getpid(), getppid()); 
             wait(NULL);  
             break; 
         } 
-    }
-
-    return 0; 
-}
+*/
